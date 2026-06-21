9fc3dfb9bddc3f46193191af461dfbacc882d155
dan7el <wdaniel.garc7a@gmail.com>
2026-06-21 10:12:42 -0500

    prepare render deployment

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -8,10 +8,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: [
-      'http://localhost:5173',
-      process.env.FRONTEND_URL || '',
-    ],
+    origin: true,
     credentials: true,
   });
 
